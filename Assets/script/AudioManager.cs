b5f15d326bf17cd4b85737c1025e9e17136a9d37
masanori-inukai <guitarist@hotmail.co.jp>
2017-04-08 23:10:11 +0900

    game終了処理

--- Assets/script/AudioManager.cs
+++ Assets/script/AudioManager.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
 
+	public GameObject gameClear;
+	public GameObject gameOver;
+
 	// Use this for initialization
 	void Start () {
 		this.gameObject.GetComponent<AudioSource> ().PlayDelayed (2.48f);
+
+		this.gameObject.GetComponent<AudioSource> ().time =
+			this.gameObject.GetComponent<AudioSource> ().clip.length - 1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!this.gameObject.GetComponent<AudioSource> ().isPlaying) {
+			if (GameData.gagePoint >= 75) {
+				this.gameClear.SetActive (true);
+			} else {
+				this.gameOver.SetActive (true);
+			}
+
+			this.enabled = false;
+		}
 	}
 }
---
+++ Assets/script/GageHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GageHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	public void setGage(float point) {
+		float length = point / 100f;
+		StopCoroutine ("GageAnimation");
+		StartCoroutine (
+			this.GageAnimation(
+				this.transform.localScale.x,
+				length,
+				0.2f
+			)
+		);
+	}
+
+	private IEnumerator GageAnimation(float start, float end, float time) {
+		float startTime = TimeManager.time;
+		float endTime = startTime + time;
+		do {
+			float t = (TimeManager.time - startTime) / time;
+			float updateValue = ((end - start) * t + start);
+			this.transform.localScale = new Vector3(updateValue, 1, 1);
+			yield return null;
+		} while (TimeManager.time < endTime);
+
+		this.transform.localScale = new Vector3(end, 1, 1);
+	}
+}
