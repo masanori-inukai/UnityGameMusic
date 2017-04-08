16886c98bda5133d62884d49e379bf7544302999
masanori-inukai <guitarist@hotmail.co.jp>
2017-04-08 13:58:55 +0900

    譜面のスクリプト追加

---
+++ Assets/ScoreHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// ゲームオブジェクトを削除する
+	public void OnScoreClick() {
+		Destroy (this.gameObject);
+	}
+}
