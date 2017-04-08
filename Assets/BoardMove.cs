5cfff127e8c040474850c463cc4245da4e3f89ff
masanori-inukai <guitarist@hotmail.co.jp>
2017-04-08 13:34:35 +0900

    譜面を動かすスクリプト作成

---
+++ Assets/BoardMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardMove : MonoBehaviour {
+
+	// 時間管理
+	public float time;
+
+	// Use this for initialization
+	void Start () {
+		this.time = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.time += Time.deltaTime;
+		this.transform.localPosition = new Vector3 (0, -this.time * 100, 0);
+	}
+}
